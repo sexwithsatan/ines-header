5899e1b1c328929e03d1621c14ecf7cdeb0bb4e2
sex with satan <sexwithsatan@users.sf.net>
2018-12-13 12:46:24 -0800

    WIP

--- src/deserialize.js
+++ src/deserialize.js
@@ -1,36 +1,36 @@
 /* globals Object */
 
-const masks = Object.freeze({
-  FOUR_SCREEN_MODE:     0b00001000,
-  TRAINER:              0b00000100,
-  PERSISTENT_MEMORY:    0b00000010,
-  MIRRORING_DIRECTION:  0b00000001
+const mask = Object.freeze({
+  FOUR_SCREEN_MODE:     0b1000,
+  TRAINER:              0b0100,
+  PERSISTENT_MEMORY:    0b0010,
+  MIRRORING_DIRECTION:  0b0001
 })
 
 export default
-function deserialize([,,,,prg,chr,flags]) {
+function deserialize([,,,,program,graphics,flags]) {
   return Object.freeze({
     size: Object.freeze({
-      program: prg * 16384,   // 16-KiB PRG-ROM banks
-      graphics: chr * 8192    //  8-KiB CHR-ROM banks
+      program: program << 14,     // 16-KiB PRG-ROM banks
+      graphics: graphics << 13    // 8-KiB CHR-ROM banks
     }),
 
     // iNES mapper number
-    mapper: (flags >> 4) & 0x0f,
+    mapper: (flags >> 4) & 0b1111,
 
-    mirroring: (flags & masks.FOUR_SCREEN_MODE)
+    mirroring: (flags & mask.FOUR_SCREEN_MODE)
       ? undefined
-      : (flags & masks.MIRRORING_DIRECTION)
+      : (flags & mask.MIRRORING_DIRECTION)
         ? 'vertical'          // CIRAM A10 = PPU A11
         : 'horizontal',       // CIRAM A10 = PPU A10
 
     // 4 KiB of RAM at PPU $2000-2FFF
-    hasFourScreenMode: !!(flags & masks.FOUR_SCREEN_MODE),
+    hasFourScreenMode: !!(flags & mask.FOUR_SCREEN_MODE),
 
     // 512-byte trainer at $7000-71FF
-    hasTrainer: !!(flags & masks.TRAINER),
+    hasTrainer: !!(flags & mask.TRAINER),
 
     // Battery-backed PRG-RAM at $6000-7FFF
-    hasPersistentMemory: !!(flags & masks.PERSISTENT_MEMORY)
+    hasPersistentMemory: !!(flags & mask.PERSISTENT_MEMORY)
   })
 }
